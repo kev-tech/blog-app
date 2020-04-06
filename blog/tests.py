b2ee0d210bddb747f2c6420c983fdecf1bdbb732
KC <kc@kevtech.co>
2020-04-05 23:09:29 -0400

    Add tests for string_representation, post_content, post_list_view, and post_details_view.

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,44 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+from .models import Post
 
-# Create your tests here.
+class BlogTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='testuser@email.com',
+            password='secret'
+        )
+
+        self.post = Post.objects.create(
+            title='Test Title',
+            body='Test content.',
+            author=self.user
+        )
+
+    def test_string_representation(self):
+        post = Post(title='Test Title')
+        self.assertEqual(str(post), post.title)
+
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'Test Title')
+        self.assertEqual(f'{self.post.author}', 'testuser')
+        self.assertEqual(f'{self.post.body}', 'Test content.')
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test content.')
+        self.assertTemplateUsed(response, 'home.html')
+    
+    def test_post_details_view(self):
+        response = self.client.get('/post/1/')
+        no_response = self.client.get('/post/1000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'Test Title')
+        self.assertTemplateUsed(response, 'post_details.html')
+
+
+
