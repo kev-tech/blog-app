1bd2d2ad3be923666e6b633bf8335a0a46022ff0
KC <kc@kevtech.co>
2020-04-13 22:14:03 -0400

    Add CRUD functionality to post DB.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailsView
+from .views import (
+    BlogListView, 
+    BlogDetailsView, 
+    BlogCreateView, 
+    BlogUpdateView,
+    BlogDeleteView
+)
 
 urlpatterns = [
-    path('post/<int:pk>/', BlogDetailsView.as_view(), name='post-details'),
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/', BlogDetailsView.as_view(), name='post_details'),
     path('', BlogListView.as_view(), name='home'),
 ]
